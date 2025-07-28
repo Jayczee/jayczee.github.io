2a2cfbc4254ab9380ee7c58ceb0f7e35a3498692
Jayczee <jayczee@yeah.net>
2025-07-28 16:42:42 +0800

    💄 nav bar transparent

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,8 +1,11 @@
 import { defineClientConfig } from "vuepress/client";
 import Popper from "./components/Popper.vue";
+import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
 
 export default defineClientConfig({
   enhance({ app, router, siteData }) {},
-  setup() {},
+  setup() {
+    setupTransparentNavbar({ type: "homepage" });
+  },
   rootComponents: [Popper],
 });
